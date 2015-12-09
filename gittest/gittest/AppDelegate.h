d0eb1076ab5cf50916c15f38365807bc1c8fa8ef
xusanshao <hbyskill@163.com>
2015-12-09 09:56:01 +0800

    yidici
    提交第一次的

---
+++ gittest/gittest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gittest
+//
+//  Created by qianfeng on 15/12/9.
+//  Copyright © 2015年 sanshao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gittest/gittest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gittest
+//
+//  Created by qianfeng on 15/12/9.
+//  Copyright © 2015年 sanshao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
